7a8a9554b0da712e03acdf44380655c3248f3b0e
Ariel Estevam <inovanota@gmail.com>
2020-08-20 17:17:10 -0300

    Drawer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,3 +4,23 @@ btn.addEventListener('click', function () {
     let drawer = document.getElementById('drawer');
     (drawer.style.right === '0px') ? (drawer.style.right = '-350px') : (drawer.style.right = '0px');
 });
+
+//Onload página layout
+function Page() {
+
+    //Renderizando as informações do Drawer conforme a página específica:
+    let page = window.location.href;    
+    switch (page) {
+        case 'https://localhost:44339/ContasPagar':
+            document.getElementById('drawer').innerHTML = "";
+            document.getElementById('drawer').innerHTML = "Página Contas a Pagar";
+            break;
+        case 'https://localhost:44339/':
+            document.getElementById('drawer').style.display = 'none';            
+            document.getElementById('bread_menu').style.display = 'none';            
+
+            break;
+        default:
+            document.getElementById('drawer').innerHTML = "";                        
+    }
+}
