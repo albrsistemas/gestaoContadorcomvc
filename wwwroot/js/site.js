411f91fc3e0746eca55bd0393830796f9647a4f7
Ariel Estevam <inovanota@gmail.com>
2020-08-20 16:49:56 -0300

    Adicionar arquivos de projeto.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿//Funcionamento do Drawer
+let btn = document.getElementById('bread_menu');
+btn.addEventListener('click', function () {
+    let drawer = document.getElementById('drawer');
+    (drawer.style.right === '0px') ? (drawer.style.right = '-350px') : (drawer.style.right = '0px');
+});
