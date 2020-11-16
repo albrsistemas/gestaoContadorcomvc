47521c69b020fb8cefb819036a03146e3f609d9c
Ariel Estevam <inovanota@gmail.com>
2020-11-16 16:00:48 -0300

    Conta corrente movimento

---
+++ Controllers/ContaCorrenteMovController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gestaoContadorcomvc.Models;
+using gestaoContadorcomvc.Models.Autenticacao;
+using gestaoContadorcomvc.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace gestaoContadorcomvc.Controllers
+{
+    public class ContaCorrenteMovController : Controller
+    {
+        // GET: ContaCorrenteMovController
+        public ActionResult Index()
+        {
+            Usuario usuario = new Usuario();
+            Vm_usuario user = new Vm_usuario();
+            user = usuario.BuscaUsuario(Convert.ToInt32(HttpContext.User.Identity.Name));
+
+            Selects select = new Selects();
+            ViewBag.ccorrente = select.getContasCorrente(user.usuario_conta_id).Select(c => new SelectListItem() { Text = c.text, Value = c.value, Disabled = c.disabled });
+            DateTime today = DateTime.Today;
+            TempData["dataInicio"] = today;
+            TempData["dataFim"] = today.AddDays(-30);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(DateTime dataInicio, DateTime dataFim, int contacorrente_id, IFormCollection d)
+        {
+            try
+            {
+                Usuario usuario = new Usuario();
+                Vm_usuario user = new Vm_usuario();
+                user = usuario.BuscaUsuario(Convert.ToInt32(HttpContext.User.Identity.Name));
+
+                Conta_corrente_mov ccm = new Conta_corrente_mov();
+                Vm_conta_corrente_mov vm_ccm = new Vm_conta_corrente_mov();
+                vm_ccm.user = user;
+                vm_ccm.conta_corrente_movimento = ccm.listaCCM(user.usuario_id, user.usuario_conta_id, contacorrente_id, dataInicio, dataFim);
+
+                Selects select = new Selects();
+                ViewBag.ccorrente = select.getContasCorrente(user.usuario_conta_id).Select(c => new SelectListItem() { Text = c.text, Value = c.value, Disabled = c.disabled, Selected = c.value == contacorrente_id.ToString() });
+                TempData["dataInicio"] = dataInicio;
+                TempData["dataFim"] = dataFim;
+
+                return View(vm_ccm);
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));                
+            }
+        }
+    }
+}
--- Models/Conta_corrente_mov.cs
+++ Models/Conta_corrente_mov.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using gestaoContadorcomvc.Models.SoftwareHouse;
+using gestaoContadorcomvc.Models.ViewModel;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Crmf;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace gestaoContadorcomvc.Models
 {
@@ -16,5 +20,115 @@ namespace gestaoContadorcomvc.Models
         public DateTime ccm_data { get; set; }
         public DateTime ccm_dataCriacao { get; set; }
         public Decimal ccm_valor { get; set; }
+
+        /*--------------------------*/
+        //Métodos para pegar a string de conexão do arquivo appsettings.json e gerar conexão no MySql.      
+        public IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            return builder.Build();
+        }
+        //Método para gerar a conexão
+        MySqlConnection conn;
+        public Conta_corrente_mov()
+        {
+            var configuration = GetConfiguration();
+            conn = new MySqlConnection(configuration.GetSection("ConnectionStrings").GetSection("conexaocvc").Value);
+        }
+
+        //MÉTODOS
+        //objeto de log para uso nos métodos
+        Log log = new Log();
+
+        //Lista movimento conta corrente
+        public List<Vm_conta_corrente_mov> listaCCM(int usuario_id, int conta_id, int contacorrente_id, DateTime dataInicio, DateTime dataFim)
+        {   
+            List<Vm_conta_corrente_mov> ccms = new List<Vm_conta_corrente_mov>();
+
+            conn.Open();
+            MySqlCommand comando = conn.CreateCommand();
+            MySqlTransaction Transacao;
+            Transacao = conn.BeginTransaction();
+            comando.Connection = conn;
+            comando.Transaction = Transacao;
+
+            try
+            {
+                comando.CommandText = "call pr_fluxoCaixa(@conta_id,@contacorrente_id,@dataInicio,@dataFim);";
+                comando.Parameters.AddWithValue("@conta_id", conta_id);
+                comando.Parameters.AddWithValue("@contacorrente_id", contacorrente_id);
+                comando.Parameters.AddWithValue("@dataInicio", dataInicio);
+                comando.Parameters.AddWithValue("@dataFim", dataFim);
+                Transacao.Commit();
+
+                var leitor = comando.ExecuteReader();
+
+                if (leitor.HasRows)
+                {
+                    while (leitor.Read())
+                    {
+                        Vm_conta_corrente_mov mov = new Vm_conta_corrente_mov();
+
+                        if (DBNull.Value != leitor["abertura"])
+                        {
+                            mov.abertura = Convert.ToDecimal(leitor["abertura"]);
+                        }
+                        else
+                        {
+                            mov.abertura = 0;
+                        }
+
+                        if (DBNull.Value != leitor["data"])
+                        {
+                            mov.data = Convert.ToDateTime(leitor["data"]);
+                        }
+                        else
+                        {
+                            mov.data = new DateTime();
+                        }
+
+                        mov.categoria = leitor["categoria"].ToString();
+                        mov.memorando = leitor["memorando"].ToString();
+                        mov.participante = leitor["cliente_fornecedor"].ToString();
+
+                        if (DBNull.Value != leitor["valor"])
+                        {
+                            mov.valor = Convert.ToDecimal(leitor["valor"]);
+                        }
+                        else
+                        {
+                            mov.valor = 0;
+                        }
+
+                        if (DBNull.Value != leitor["saldo"])
+                        {
+                            mov.saldo = Convert.ToDecimal(leitor["saldo"]);
+                        }
+                        else
+                        {
+                            mov.saldo = 0;
+                        }
+
+                        ccms.Add(mov);
+                    }                    
+                }               
+            }
+            catch (Exception e)
+            {
+                string msg = e.Message.Substring(0, 300);
+                log.log("Conta_corrente_mov", "Vm_conta_corrente_mov", "Erro", msg, conta_id, usuario_id);
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+            return ccms;
+        }
+
+
     }
 }
