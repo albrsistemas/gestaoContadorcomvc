9bde28bbd7c0690ed45e0af73e58e7128c672984
Ariel Estevam <inovanota@gmail.com>
2020-12-30 15:42:56 -0300

    Ajustes layout tabelas contas e relatório

--- Areas/Contabilidade/Controllers/HomeController.cs
+++ Areas/Contabilidade/Controllers/HomeController.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading;
-using System.Threading.Tasks;
 using gestaoContadorcomvc.Areas.Contabilidade.Models;
 using gestaoContadorcomvc.Areas.Contabilidade.Models.ViewModel;
-using gestaoContadorcomvc.Filtros;
 using gestaoContadorcomvc.Models.Autenticacao;
 using gestaoContadorcomvc.Models.ViewModel;
 using Microsoft.AspNetCore.Authorization;
@@ -15,9 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace gestaoContadorcomvc.Areas.Contabilidade.Controllers
 {
     [Area("Contabilidade")]
-    [Route("Contabilidade/[controller]/[action]")]
-    //[FiltroAutenticacao]
-    //[FiltroContabilidade]
+    [Route("Contabilidade/[controller]/[action]")]    
     [Authorize(Roles = "Contabilidade")]    
     public class HomeController : Controller
     {
@@ -27,7 +19,7 @@ namespace gestaoContadorcomvc.Areas.Contabilidade.Controllers
             Vm_usuario user = new Vm_usuario();
             user = usuario.BuscaUsuario(Convert.ToInt32(HttpContext.User.Identity.Name));
 
-            Vm_home home = new Vm_home();
+            Vm_home_contabilidade home = new Vm_home_contabilidade();
             home.user = user;
 
             return View(home);
--- Models/ViewModel/Vm_home.cs
+++ Models/ViewModel/Vm_home.cs
@@ -1,10 +1,9 @@
-﻿using gestaoContadorcomvc.Models.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace gestaoContadorcomvc.Areas.Contabilidade.Models.ViewModel
+namespace gestaoContadorcomvc.Models.ViewModel
 {
     public class Vm_home
     {
