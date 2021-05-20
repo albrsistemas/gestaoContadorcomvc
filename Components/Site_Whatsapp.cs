1c1c806b3bea1f6bfa2cdad63264d948ad347c5f
Ariel Estevam <inovanota@gmail.com>
2021-05-20 18:03:41 -0300

    lead site whatsapp

--- Components/Site_Whatsapp.cs
+++ Components/Site_Whatsapp.cs
@@ -18,7 +18,11 @@ namespace gestaoContadorcomvc.Components
             Codificacao c = new Codificacao();
             var codigo = c.EncodeToBase64(l.lead_atendentes_id.ToString() + "," + user_id);
 
-            TempData["msgParticipante"] = codigo;
+            string w_link = "https://api.whatsapp.com/send?phone=55" + l.lead_atendentes_celular + "&text=Ol%C3%A1%2C%20tudo%20bem%3F%20Gostaria%20de%20conversar%20sobre%20os%20servi%C3%A7os%20da%20Contadorcomvoc%C3%AA";
+
+            TempData["codigo"] = codigo;
+            TempData["link"] = w_link;
+            TempData["nome"] = l.lead_atendentes_nome;
 
             return View();
         }
